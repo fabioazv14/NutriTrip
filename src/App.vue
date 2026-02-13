c192aa47c57eb97a5381ca4ba348e4e760f94cd5
fabioazv14 <fabio.azv14@gmail.com>
2026-02-13 22:29:20 +0000

    feat: add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,36 @@
 <script setup>
+import { RouterView, RouterLink } from 'vue-router'
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
+
+const route = useRoute()
+const showNavbar = computed(() => !route.meta.hideNavbar)
 </script>
 
 <template>
-  <div>
-    <h1>Teste</h1>
-  </div>
+  <nav v-if="showNavbar">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/login">Login</RouterLink>
+  </nav>
+  <RouterView />
 </template>
+
+<style scoped>
+nav {
+  display: flex;
+  gap: 16px;
+  padding: 16px;
+  background-color: #f5f5f5;
+}
+
+nav a {
+  text-decoration: none;
+  color: #333;
+  font-weight: 500;
+}
+
+nav a.router-link-active {
+  color: #42b883;
+  font-weight: 700;
+}
+</style>
